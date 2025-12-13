76480c44a96bd89999fa7b2fb6eae16b1921e92c
sekarasiewicz <sekarasiewicz@gmail.com>
2025-12-13 19:55:11 +0100

    Initial Commit

---
+++ terma/termaApp.swift
@@ -0,0 +1,17 @@
+//
+//  termaApp.swift
+//  terma
+//
+//  Created by Sebastian Karasiewicz on 13/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct termaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ termaTests/termaTests.swift
@@ -0,0 +1,17 @@
+//
+//  termaTests.swift
+//  termaTests
+//
+//  Created by Sebastian Karasiewicz on 13/12/2025.
+//
+
+import Testing
+@testable import terma
+
+struct termaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
